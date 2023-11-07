42a4c922cf6e0fa42753b7e5f6611979f852ef5b
Akshaypatel1812 <pak180301@gmail.com>
2023-11-07 19:09:47 +0530

    add basics file

---
+++ basic.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+
+public class basic {
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+        Scanner input=new Scanner(System.in);
+        System.out.println(input);
+    }
+}
