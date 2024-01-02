6f2e48f7cced954eb7c7b0e3db1509b66710d721
Akshaypatel1812 <pak180301@gmail.com>
2024-01-02 23:32:11 +0530

    change bubblesortRecursion

--- bubblesortRecursion.java
+++ bubblesortRecursion.java
@@ -3,22 +3,22 @@ import java.util.Arrays;
 public class bubblesortRecursion {
 
     static void BSR(int[] a, int i, int j) {
-        if (i == a.length - 1) {
+        if (i==a.length-1) {
             return;
         }
 
-        if (j < a.length - i) {
-            if(a[j-1]>a[j]){
+        if (j < a.length-1) {
+            if(a[j]>a[j+1]){
                 int temp=a[j];
                 a[j]=a[j+1];
                 a[j+1]=temp;
             }
              
-            BSR(a,i,j++);
+            BSR(a,i,j+1);
         }
         else{  
-
-            BSR(a,i++,j);
+            
+            BSR(a,i+1,0);
         }
         
        
@@ -26,7 +26,7 @@ public class bubblesortRecursion {
 
     public static void main(String[] args) {
         int[] arr = { 10, 4, 12, 45, 2, 6, 18 };
-        BSR(arr, 0, 1);
+        BSR(arr, 0, 0);
         System.out.println(Arrays.toString(arr));
     }
 }
