4200bfd926fbd3b86aa78ebff9f2bd93777c6c41
Akshaypatel1812 <pak180301@gmail.com>
2024-01-09 20:40:41 +0530

    add string problem which print only even string

---
+++ StringEvenWord.java
@@ -0,0 +1,18 @@
+public class StringEvenWord {
+
+    static void printEvenWord(String s) {
+        for (String s1 : s.split(" ")) {
+            if (s1.length() % 2 == 0) {
+                System.out.println(s1);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        String str = "akshay kiritbhaig patel";
+
+        printEvenWord(str);
+
+    }
+}
