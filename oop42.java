0bb4a8c4b4826284e6dcc5186cf80db0ac956610
Akshaypatel1812 <pak180301@gmail.com>
2024-01-11 17:39:48 +0530

    add some instructions for method overriding

--- oop42.java
+++ oop42.java
@@ -1,7 +1,15 @@
-//here we code the method overriding in Dynamic polymorphism
-class test {
+
+/*here we code the method overriding in Dynamic polymorphism
+ * method overriding requires 4 things 
+ * 1.same mathod name
+ * 2.different class
+ * 3.same argument
+ * 4.inheritance or IS-A relationship
+ */
+class test {  //here after version 1.4 of java return type not required to same but in overriding child class return type should be a sub type of parents return type
     void show() {
         System.out.println("test class method");
+        
     }
 }
 
@@ -12,10 +20,10 @@ public class oop42 extends test {
     }
 
     public static void main(String[] args) {
-        test t1=new test();
+        test t1 = new test();
         t1.show();
 
-        oop42 obj=new oop42();
-        obj.show();
+        oop42 obj = new oop42();
+        obj.show(); // here objects calls the methods according to their classes
     }
 }
