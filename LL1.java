f6beb6e5acc5ecafba2c8629a45284e10fea6127
Akshaypatel1812 <pak180301@gmail.com>
2024-01-13 00:43:22 +0530

    add linked list first program

---
+++ LL1.java
@@ -0,0 +1,97 @@
+
+
+public class LL1 {
+    node head;
+
+    class node {
+        int data;
+        node next;
+
+        node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    // add-first
+    public void addfirst(int data) {
+        node newnode = new node(data);
+        if (head == null) {
+            head = newnode;
+            return ;
+        }
+
+        newnode.next = head;
+        head = newnode;
+
+    }
+
+    // add-last
+    public void addlast(int data) {
+        node newnode = new node(data);
+        if (head == null) {
+            head = newnode;
+            return ;
+        }
+
+        node tempnode = head;
+        while (tempnode.next != null) {
+            tempnode = tempnode.next;
+
+        }
+        tempnode.next = newnode;
+
+    }
+
+    // add-mid
+    public void addmid(int data,int index){
+        node newnode = new node(data);
+        if (head == null) {
+            head = newnode;
+            return ;
+        }
+        int i=1;
+        node tempnode = head;
+        while (i != index) {
+            tempnode = tempnode.next;
+            i++;
+        }
+
+        newnode.next=tempnode.next;
+        tempnode.next=newnode;
+    }
+
+    // print linked list
+
+    public void printLL() {
+
+        if (head == null) {
+            System.out.println("list is empty");
+            return ;
+        }
+        node tempnode = head;
+        while (tempnode != null) {
+            System.out.print(tempnode.data +"->");
+            tempnode = tempnode.next;
+        }
+
+        System.out.println("NULL");
+
+    }
+
+    public static void main(String[] args) {
+        LL1 l1 = new LL1();
+
+        l1.addfirst(2);
+        l1.addlast(4);
+        l1.printLL();
+        l1.addfirst(1);
+        l1.addmid(3, 2);
+
+        l1.printLL();
+        
+       
+
+
+    }
+}
