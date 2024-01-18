8a740738186842087acf4c5df6035cd07993631d
Akshaypatel1812 <pak180301@gmail.com>
2024-01-18 23:07:26 +0530

    Add doubly Link List

---
+++ LL3.java
@@ -0,0 +1,120 @@
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode prev;
+
+    ListNode(int val){
+       this.val=val;
+       this.next=null;
+       this.prev=null;
+    } 
+}
+
+public class LL3 {
+    public static ListNode head;
+    public static ListNode tail;
+    public static int size;
+
+    //add first in doublyLL
+    public static void addfirst(int val){
+        ListNode newNode=new ListNode(val);
+        size++;
+        if(head==null){
+            head=newNode;
+            return;
+        }
+
+        newNode.next=head;
+        head.prev=newNode;
+        head=newNode;
+
+    }
+
+    //add last
+    public static void addlast(int val){
+        ListNode newNode=new ListNode(val);
+        size++;
+        if(head==null){
+            head=newNode;
+            return;
+        }
+        ListNode tempNode=head;
+        while(tempNode.next!=null){
+            tempNode=tempNode.next;
+        }
+
+        tempNode.next=newNode;
+        newNode.next=null;
+        newNode.prev=tempNode;
+
+    }
+
+
+    //remove last
+    public static void removelast(int val){
+        ListNode newNode=new ListNode(val);
+        if(head==null){
+            head=newNode;
+        }
+
+        ListNode tempNode=head;
+        while(tempNode.next.next!=null){
+            tempNode=tempNode.next;
+        }
+        tempNode.next=null;
+        // System.out.println(tempNode.val);
+
+    }
+
+    //remove first
+    public static void removefirst(){
+        if(head==null){
+            System.out.println("Link list is empty");
+            return ;
+        }
+
+        ListNode tempNode=head.next;
+        tempNode.prev=null;
+        head=tempNode;
+    }
+
+    //print LinkList
+    public static void printLL(){
+        if(head==null){
+            System.out.println("link list is empty");
+            return;
+        }
+        ListNode tempNode=head;
+        while(tempNode!=null){
+            System.out.print(tempNode.val+"->");
+            tempNode=tempNode.next;
+        }
+
+        System.out.println("null");
+
+    }
+    public static void main(String[] args) {
+        LL3 l1=new LL3();
+        l1.addfirst(5);
+        l1.addfirst(4);
+        l1.addfirst(3);
+        l1.addfirst(2);
+        l1.addfirst(1);
+
+        l1.printLL();
+
+        l1.removefirst();
+
+        l1.printLL();
+
+        l1.removelast(size);
+
+        l1.printLL();
+
+        l1.addlast(6);
+
+        l1.printLL();
+
+
+    }
+}
